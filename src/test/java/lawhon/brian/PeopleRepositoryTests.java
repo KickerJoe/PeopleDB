3675f7438e87039f2b3b61142370e59f5e7543ea
Brian Lawhon <brianlawhon@live.com>
2022-08-28 21:09:19 -0500

    Setup and implemented first test method

---
+++ src/test/java/lawhon/brian/PeopleRepositoryTests.java
@@ -0,0 +1,16 @@
+package lawhon.brian;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class PeopleRepositoryTests {
+
+    @Test
+    public void canSave() {
+        PeopleRepository repo = new PeopleRepository();
+        Person john = new Person("John", "Smith", ZonedDateTime.of(1980, 11, 15, 15, 15, 0, 0, ZoneId.of("-6")));
+        repo.save(john);
+    }
+}
