a20d0e75c3001900256fbf5172fe5f54440797a5
Brian Lawhon <brianlawhon@live.com>
2022-08-30 15:13:50 -0500

    Set up second test method that test for 2 saved persons. Included connection to h2 database.

--- src/main/java/lawhon/brian/PeopleRepository.java
+++ src/main/java/lawhon/brian/PeopleRepository.java
@@ -1,6 +1,12 @@
 package lawhon.brian;
 
+import java.sql.Connection;
+
 public class PeopleRepository {
+    private Connection connection;
+    public PeopleRepository(Connection connection) {
+        this.connection = connection;
+    }
 
     public Person save(Person person) {
         return person;
