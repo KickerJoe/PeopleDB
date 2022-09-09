8901d825e4eb524b57eaad2b25f04c00b27429fa
Brian Lawhon <brianlawhon@live.com>
2022-09-09 16:25:08 -0500

    Refactored the remaining CRUD methods.

--- src/main/java/lawhon/brian/CRUDRepository.java
+++ src/main/java/lawhon/brian/CRUDRepository.java
@@ -1,16 +1,18 @@
 package lawhon.brian;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import static java.util.stream.Collectors.joining;
 
 abstract class CRUDRepository<T extends Entity> {
 
     protected Connection connection;
-
     public CRUDRepository(Connection connection) {
         this.connection = connection;
     }
-
     public T save(T entity) {
         try {
             PreparedStatement ps = connection.prepareStatement(getSaveSql(), Statement.RETURN_GENERATED_KEYS);
@@ -28,7 +30,6 @@ abstract class CRUDRepository<T extends Entity> {
         }
         return entity;
     }
-
     public Optional<T> findById(Long id) {
         T entity = null;
 
@@ -44,9 +45,87 @@ abstract class CRUDRepository<T extends Entity> {
         }
         return Optional.ofNullable(entity);
     }
+    public List<T> findAll() {
+        List<T> entities = new ArrayList<>();
+        try {
+            PreparedStatement ps = connection.prepareStatement(getFindAllSql());
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                entities.add(extractEntityFromResultSet(rs));
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return entities;
+    }
+    public long count(){
+        long count = 0;
+        try{
+            PreparedStatement ps = connection.prepareStatement(getCountSql());
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                count = rs.getLong(1);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return count;
+    }
+    public void delete(T entity) {
+        try {
+            PreparedStatement ps = connection.prepareStatement(getDeleteSql());
+            ps.setLong(1, entity.getId());
+            int affectedRecordCount = ps.executeUpdate();
+            System.out.println(affectedRecordCount);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Below method is bad programming, SQL injection. Avoid using a createStatement
+    //Example purpose only. This will delete records all at the same time, instead of one by one
+    public void delete(T...entities) {
+        try {
+            Statement stmt = connection.createStatement();
+            String ids = Arrays.stream(entities).map(T::getId).map(String::valueOf).collect(joining(","));
+            int affectedRecordCount = stmt.executeUpdate(getDeleteInSql().replace(":ids", ids));
+            System.out.println(affectedRecordCount);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    /*
+    The following code could replace the above. It will delete a person one at a time.
+    This makes use of the delete method for removing one person.
+        for (Person person : people){
+            delete(person);
+        }
+     */
+    public void update(T entity) {
+        try {
+            PreparedStatement ps = connection.prepareStatement(getUpdateSql());
+            mapForUpdate(entity, ps);
+            ps.setLong(5, entity.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     *
+     * @return Should return a SQL string like:
+     * "DELETE FROM PEOPLE WHERE ID IN (:ids)"
+     * Be sure to include the '(:ids) named parameter & call it 'ids'
+     */
+    protected abstract String getDeleteInSql();
+    protected abstract String getUpdateSql();
+    protected abstract String getDeleteSql();
+
+    protected abstract String getCountSql();
+
+    protected abstract String getFindAllSql();
 
     abstract T extractEntityFromResultSet(ResultSet rs) throws SQLException;
-
     /**
      *
      * @return Returns a String that represents the SQL needed to retrieve one entity.
@@ -56,6 +135,6 @@ abstract class CRUDRepository<T extends Entity> {
     abstract String getFindByIdSql();
 
     abstract void mapForSave(T entity, PreparedStatement ps) throws SQLException;
-
+    abstract void mapForUpdate(T entity, PreparedStatement ps) throws SQLException;
     abstract String getSaveSql();
 }
--- src/main/java/lawhon/brian/PeopleRepository.java
+++ src/main/java/lawhon/brian/PeopleRepository.java
@@ -4,39 +4,60 @@ import java.math.BigDecimal;
 import java.sql.*;
 import java.time.ZoneId;
 import java.time.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
-import static java.util.stream.Collectors.joining;
 
 public class PeopleRepository extends CRUDRepository<Person>{
     public static final String SAVE_PERSON_SQL = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES(?, ?, ?)";
     public static final String FIND_BY_ID_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE WHERE ID=?";
     public static final String FIND_ALL_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE";
+    public static final String SELECT_COUNT_SQL = "SELECT COUNT(*) FROM PEOPLE";
+    public static final String DELETE_SQL = "DELETE FROM PEOPLE WHERE ID=?";
+    public static final String DELETE_IN_SQL = "DELETE FROM PEOPLE WHERE ID IN (:ids)";
+    private static final String UPDATE_SQL = "UPDATE PEOPLE SET FIRST_NAME=?, LAST_NAME=?, DOB=?, SALARY=? WHERE ID=?";
 
     public PeopleRepository(Connection connection) {
         super(connection);
     }
-
     @Override
     String getSaveSql() {
         return SAVE_PERSON_SQL;
     }
-
     @Override
     String getFindByIdSql() {
         return FIND_BY_ID_SQL;
     }
-
+    @Override
+    protected String getFindAllSql() {
+        return FIND_ALL_SQL;
+    }
+    @Override
+    protected String getCountSql() {
+        return SELECT_COUNT_SQL;
+    }
+    @Override
+    protected String getDeleteSql() {
+        return DELETE_SQL;
+    }
+    @Override
+    protected String getDeleteInSql() {
+        return DELETE_IN_SQL;
+    }
+    @Override
+    protected String getUpdateSql() {
+        return UPDATE_SQL;
+    }
+    @Override
+    void mapForUpdate(Person entity, PreparedStatement ps) throws SQLException {
+        ps.setString(1, entity.getFirstName());
+        ps.setString(2, entity.getLastName());
+        ps.setTimestamp(3, convertDobToTimestamp(entity.getDob()));
+        ps.setBigDecimal(4, entity.getSalary());
+    }
     @Override
     void mapForSave(Person entity, PreparedStatement ps) throws SQLException {
         ps.setString(1, entity.getFirstName());
         ps.setString(2, entity.getLastName());
         ps.setTimestamp(3, convertDobToTimestamp(entity.getDob()));
     }
-
     @Override
     Person extractEntityFromResultSet(ResultSet rs) throws SQLException{
         long personId = rs.getLong("ID");
@@ -46,82 +67,7 @@ public class PeopleRepository extends CRUDRepository<Person>{
         BigDecimal salary = rs.getBigDecimal("SALARY");
         return new Person(personId, firstName, lastName, dob, salary);
     }
-
     private static Timestamp convertDobToTimestamp(ZonedDateTime dob) {
         return Timestamp.valueOf(dob.withZoneSameInstant(ZoneId.of("+0")).toLocalDateTime());
     }
-
-    public List<Person> findAll() {
-        List<Person> people = new ArrayList<>();
-        try {
-            PreparedStatement ps = connection.prepareStatement(FIND_ALL_SQL);
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()){
-                people.add(extractEntityFromResultSet(rs));
-            }
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
-        return people;
-    }
-
-    public long count(){
-        long count = 0;
-        try{
-            PreparedStatement ps = connection.prepareStatement("SELECT COUNT(*) FROM PEOPLE");
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                count = rs.getLong(1);
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return count;
-    }
-
-    public void delete(Person person) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("DELETE FROM PEOPLE WHERE ID=?");
-            ps.setLong(1, person.getId());
-            int affectedRecordCount = ps.executeUpdate();
-            System.out.println(affectedRecordCount);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //Below method is bad programming, SQL injection. Avoid using a createStatement
-    //Example purpose only. This will delete records all at the same time, instead of one by one
-    public void delete(Person...people) {
-        try {
-            Statement stmt = connection.createStatement();
-            String ids = Arrays.stream(people).map(Person::getId).map(String::valueOf).collect(joining(","));
-            int affectedRecordCount = stmt.executeUpdate("DELETE FROM PEOPLE WHERE ID IN (:ids)".replace(":ids", ids));
-            System.out.println(affectedRecordCount);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    /*
-    The following code could replace the above. It will delete a person one at a time.
-    This makes use of the delete method for removing one person.
-        for (Person person : people){
-            delete(person);
-        }
-     */
-
-    public void update(Person person) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("UPDATE PEOPLE SET FIRST_NAME=?, LAST_NAME=?, DOB=?, SALARY=? WHERE ID=?");
-            ps.setString(1, person.getFirstName());
-            ps.setString(2, person.getLastName());
-            ps.setTimestamp(3, convertDobToTimestamp(person.getDob()));
-            ps.setBigDecimal(4, person.getSalary());
-            ps.setLong(5, person.getId());
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
